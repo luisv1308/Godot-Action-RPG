6d1deea0b97ba4f5df72572b02eea068fefd56e0
Luis Velasquez <l.velasquez1308@gmail.com>
2021-07-11 02:25:11 -0600

    Terminado video 16

--- scenes/Effect.gd
+++ scenes/Effect.gd
@@ -1,12 +1,11 @@
-extends Node2D
-
-onready var animatedSprite = $AnimatedSprite
+extends AnimatedSprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	animatedSprite.frame = 0
-	animatedSprite.play("Animate")
+	connect("animation_finished", self, "_on_animation_finished")
+	frame = 0
+	play("Animate")
 
 
-func _on_AnimatedSprite_animation_finished():
+func _on_animation_finished():
 	queue_free()
